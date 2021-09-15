776974962db135446251254d66b4ceb5cefde7f7
alex-sikach <hlophlop1245@gmail.com>
2021-09-15 18:44:43 +0300

    not ready version, just for commit

---
+++ index.js
@@ -0,0 +1,65 @@
+var ans = 0;
+function clear() { /* clear */
+
+    document.querySelector('.res').value = ans;
+};
+function back() { /* <- */
+
+    document.querySelector('.res').value = ans;
+};
+function root() { /* sqrt() */
+
+    document.querySelector('.res').value = ans;
+};
+function degree() { /* degree */
+
+    document.querySelector('.res').value = ans;
+};
+function lbkt() {
+
+    document.querySelector('.res').value = ans;
+};
+function rbkt() {
+
+    document.querySelector('.res').value = ans;
+};
+function factorail() {
+
+    document.querySelector('.res').value = ans;
+};
+function pi() {
+
+    document.querySelector('.res').value = ans;
+};
+function number() {
+
+    document.querySelector('.res').value = ans;
+};
+function procents() {
+
+    document.querySelector('.res').value = ans;
+};
+function dote() {
+
+    document.querySelector('.res').value = ans;
+};
+function divide() {
+
+    document.querySelector('.res').value = ans;
+};
+function multiply() {
+
+    document.querySelector('.res').value = ans;
+};
+function plus() {
+
+    document.querySelector('.res').value = ans;
+};
+function minus() {
+
+    document.querySelector('.res').value = ans;
+};
+function equals() {
+
+    document.querySelector('.res').value = ans;
+};
